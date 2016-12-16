ccc4a697bbbbf51d05bc8ad873c4957cb526a427
crthwaite <cristofollluis.rivas@e-campus.uab.cat>
2016-12-17 00:19:23 +0100

    Force login

---
+++ src/Fone/CoreBundle/CoreBundle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: christopher
+ * Date: 17/12/16
+ * Time: 0:17
+ */
+
+namespace Fone\CoreBundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class CoreBundle extends Bundle
+{
+
+}
